6aa8bcf770e75c32c13bac1cb92bc2ceb34e3fad
pinguo-liuzhaohui <liuzhaohui@camera360.com>
2015-12-19 18:05:07 +0800

    Update mysql-init-cmd.sql

--- mysql-init-cmd.sql
+++ mysql-init-cmd.sql
@@ -1,8 +1,10 @@
 
 # change password
+# 参考 http://dev.mysql.com/doc/refman/5.7/en/alter-user.html
 alter user 'root'@'localhost' IDENTIFIED BY 'xxxx'; 
 
 # 创建用户/授权
+# 参考 http://dev.mysql.com/doc/refman/5.7/en/grant.html
 GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'localhost' IDENTIFIED BY 'xxxx';
 GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'*' IDENTIFIED BY 'xxxx';
 GRANT ALL PRIVILEGES ON db1.* TO 'userx'@'localhost' IDENTIFIED BY 'xxxx';
