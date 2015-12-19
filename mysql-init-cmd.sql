5135160422e160a079f07a68eb77291aa8a73111
pinguo-liuzhaohui <liuzhaohui@camera360.com>
2015-12-19 18:04:28 +0800

    Update mysql-init-cmd.sql

--- mysql-init-cmd.sql
+++ mysql-init-cmd.sql
@@ -2,3 +2,8 @@
 # change password
 alter user 'root'@'localhost' IDENTIFIED BY 'xxxx'; 
 
+# 创建用户/授权
+GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'localhost' IDENTIFIED BY 'xxxx';
+GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'*' IDENTIFIED BY 'xxxx';
+GRANT ALL PRIVILEGES ON db1.* TO 'userx'@'localhost' IDENTIFIED BY 'xxxx';
+
