f18aa67f5fe9b4935cf544f3a07afbb7d705e5d7
pinguo-liuzhaohui <liuzhaohui@camera360.com>
2015-12-19 18:08:10 +0800

    Update mysql-init-cmd.sql

--- mysql-init-cmd.sql
+++ mysql-init-cmd.sql
@@ -9,3 +9,5 @@ GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'localhost' IDEN
 GRANT CREATE,INSERT,SELECT,LOCK TABLES,UPDATE ON db1.* TO 'app'@'*' IDENTIFIED BY 'xxxx';
 GRANT ALL PRIVILEGES ON db1.* TO 'userx'@'localhost' IDENTIFIED BY 'xxxx';
 
+# 查看用户权限
+SHOW GRANTS FOR 'app'@'localhost';
